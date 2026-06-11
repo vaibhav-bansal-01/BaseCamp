4d489f48ae9e78bf5d6f590956479954c2bc7bd8
Vaibhav Bansal <vaibhavb11104@gmail.com>
2026-06-11 18:14:34 +0530

    add src and puclic folder, add cors

---
+++ src/app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+
+//basic configuration
+
+//read json data sent by the client
+app.use(express.json({ limit: "16kb" }));
+
+//for encoded url and decides how complex cobject can be
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+//images part should be pucblicly viewable
+app.use(express.static("public"));
+
+//cors configuration allows which frontend website are aloowed to talk to my backend
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true, //allow cookies/auth info
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Authorization", "Content-Type"],
+  }),
+);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to baseCamp");
+});
+
+export default app;
